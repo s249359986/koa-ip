63bb10ed77d486994ea0a74e4745ea680e438161
sdh <249359986@qq.com>
2016-12-09 11:41:10 +0800

    init index

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 /**
  * Created by songdonghong on 2016/12/9.
  */
+
+function ip(conf) {
+    var MSG="非法ip";
+    var PROXY="X-Real-IP";
+    if (typeof conf !== 'object') {
+        if (typeof conf === 'string') {
+            conf = {whiteList: [conf]};
+        } else {
+            conf = {
+            };
+        }
+    }
+    if (Array.isArray(conf)) {
+        conf = {whiteList: conf};
+    }
+    return function* (next) {
+        var _this=this;
+        var tempReq=_this.request;
+        var _ip = tempReq.get(conf.proxy||PROXY) || tempReq.get("X-Forwarded-For")||_this.ip;
+        var pass = false;
+        if (conf.whiteList && Array.isArray(conf.whiteList)) {
+            pass = conf.whiteList.some(function (item) {
+                return RegExp(item).test(_ip);
+            });
+        }
+        if (conf.blackList && Array.isArray(conf.blackList)) {
+            pass = !conf.blackList.some(function (item) {
+                return RegExp(item).test(_ip);
+            });
+        }
+        if (pass) {
+            console.log((new Date).toUTCString() + ' ' + _ip + ' -> ok');
+            yield next;
+        } else {
+            console.log((new Date).toUTCString() + ' ' + _ip + ' -> no');
+            _this.status=403;
+            _this.body=conf.msg||MSG;
+        }
+    }
+}
+
+module.exports = ip;
